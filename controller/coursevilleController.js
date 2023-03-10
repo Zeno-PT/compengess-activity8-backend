441e2468afc0c6227cd5097904f34fede11ba455
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-11 00:24:58 +0700

    Update coursevilleController.js

--- controller/coursevilleController.js
+++ controller/coursevilleController.js
@@ -21,7 +21,7 @@ exports.authApp = (req, res) => {
   res.redirect(authorization_url);
 };
 
-exports.accessToken = (req, res) => {
+exports.accessToken = async (req, res) => {
   const parsedUrl = url.parse(req.url);
   const parsedQuery = querystring.parse(parsedUrl.query);
 
@@ -32,83 +32,80 @@ exports.accessToken = (req, res) => {
     return;
   }
 
-  res.send(parsedQuery.code);
-  res.end();
-
   // If the user granted the authorization request
-  // if (parsedQuery.code) {
-  //   // Exchange the authorization code for an access token
-  //   const postData = querystring.stringify({
-  //     grant_type: "authorization_code",
-  //     code: parsedQuery.code,
-  //     client_id: client_id,
-  //     client_secret: client_secret,
-  //     redirect_uri: redirect_uri,
-  //   });
+  if (parsedQuery.code) {
+    // Exchange the authorization code for an access token
+    const postData = querystring.stringify({
+      grant_type: "authorization_code",
+      code: parsedQuery.code,
+      client_id: client_id,
+      client_secret: client_secret,
+      redirect_uri: redirect_uri,
+    });
 
-  // const options = {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/x-www-form-urlencoded",
-  //     "Content-Length": postData.length,
-  //   },
-  // };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": postData.length,
+      },
+    };
 
-  // const tokenReq = https.request(access_token_url, options, (tokenRes) => {
+    res.send('OK')
+    res.end()
 
-  //   let tokenData = "";
-  //   tokenRes.on("data", (chunk) => {
-  //     tokenData += chunk;
-  //   });
-  //   tokenRes.on("end", () => {
-  //     const token = JSON.parse(tokenData);
-  //     console.log(token)
-  // Use the access token to fetch the user's profile
-  // const profileOptions = {
-  //   headers: {
-  //     Authorization: `Bearer ${token.access_token}`,
-  //   },
-  // };
-  // const profileReq = https.request(
-  //   "https://www.mycourseville.com/api/v1/public/users/me",
-  //   profileOptions,
-  //   (profileRes) => {
-  //     let profileData = "";
-  //     profileRes.on("data", (chunk) => {
-  //       profileData += chunk;
-  //     });
-  //     profileRes.on("end", () => {
-  //       const profile = JSON.parse(profileData);
+    const tokenReq = https.request(access_token_url, options, (tokenRes) => {
+      let tokenData = "";
+      tokenRes.on("data", (chunk) => {
+        tokenData += chunk;
+      });
+      tokenRes.on("end", () => {
+        const token = JSON.parse(tokenData);
+        console.log(token);
+        const profileOptions = {
+          headers: {
+            Authorization: `Bearer ${token.access_token}`,
+          },
+        };
+        const profileReq = https.request(
+          "https://www.mycourseville.com/api/v1/public/users/me",
+          profileOptions,
+          (profileRes) => {
+            let profileData = "";
+            profileRes.on("data", (chunk) => {
+              profileData += chunk;
+            });
+            profileRes.on("end", () => {
+              const profile = JSON.parse(profileData);
 
-  //       // Redirect to the home page after successful authentication
-  //       res.writeHead(302, { Location: "/" });
-  //       // res.send(profile)
-  //       res.end();
-  //     });
-  //   }
-  // );
-  // profileReq.on("error", (err) => {
-  //   console.error(err);
-  // });
-  // profileReq.end();
-  //     res.send('Ended')
-  //     res.end();
-  //   });
-
-  // });
-  // tokenReq.on("error", (err) => {
-  //   console.error(err);
-  // });
-  // // tokenReq.write('success');
-  // tokenReq.end();
-  // } else {
-  //   // If the user hasn't granted or denied the authorization request yet, redirect to the authorization URL
-  //   res.writeHead(302, { Location: authorization_url });
-  //   res.end();
-  // }
+              // Redirect to the home page after successful authentication
+              res.writeHead(302, { Location: "/" });
+              // res.send(profile)
+              res.end();
+            });
+          }
+        );
+        profileReq.on("error", (err) => {
+          console.error(err);
+        });
+        profileReq.end();
+        res.send("Ended");
+        res.end();
+      });
+    });
+    tokenReq.on("error", (err) => {
+      console.error(err);
+    });
+    // tokenReq.write('success');
+    tokenReq.end();
+  } else {
+    // If the user hasn't granted or denied the authorization request yet, redirect to the authorization URL
+    res.writeHead(302, { Location: authorization_url });
+    res.end();
+  }
 };
 
-exports.getProfileInformation = (req, res) => {
+exports.getProfileInformation = async (req, res) => {
   const options = {
     headers: {
       Authorization: `Bearer ${token.access_token}`,
