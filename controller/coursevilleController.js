02c3f6dee384c583e937b7255b2ce960c068866b
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-10 21:57:51 +0700

    Update coursevilleController.js

--- controller/coursevilleController.js
+++ controller/coursevilleController.js
@@ -27,82 +27,80 @@ exports.authApp = (req, res) => {
 exports.accessToken = (req, res) => {
   const parsedUrl = url.parse(req.url);
   const parsedQuery = querystring.parse(parsedUrl.query);
-  res.send(parsedQuery)
-  res.end()
 
-  // // If the user denied the authorization request
-  // if (parsedQuery.error) {
-  //   res.writeHead(400, { "Content-Type": "text/plain" });
-  //   res.end(`Authorization error: ${parsedQuery.error_description}`);
-  //   return;
-  // }
+  // If the user denied the authorization request
+  if (parsedQuery.error) {
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    res.end(`Authorization error: ${parsedQuery.error_description}`);
+    return;
+  }
 
-  // // If the user granted the authorization request
-  // if (parsedQuery.code) {
-  //   // Exchange the authorization code for an access token
-  //   const postData = querystring.stringify({
-  //     grant_type: "authorization_code",
-  //     code: parsedQuery.code,
-  //     client_id: client_id,
-  //     client_secret: client_secret,
-  //     redirect_uri: redirect_uri,
-  //   });
+  // If the user granted the authorization request
+  if (parsedQuery.code) {
+    // Exchange the authorization code for an access token
+    const postData = querystring.stringify({
+      grant_type: "authorization_code",
+      code: parsedQuery.code,
+      client_id: client_id,
+      client_secret: client_secret,
+      redirect_uri: redirect_uri,
+    });
 
-  //   const options = {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //       "Content-Length": postData.length,
-  //     },
-  //   };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": postData.length,
+      },
+    };
 
-  //   const tokenReq = https.request(access_token_url, options, (tokenRes) => {
-  //     let tokenData = "";
-  //     tokenRes.on("data", (chunk) => {
-  //       tokenData += chunk;
-  //     });
-  //     tokenRes.on("end", () => {
-  //       const token = JSON.parse(tokenData);
-  //       // Use the access token to fetch the user's profile
-  //       const profileOptions = {
-  //         headers: {
-  //           Authorization: `Bearer ${token.access_token}`,
-  //         },
-  //       };
-  //       const profileReq = https.request(
-  //         "https://www.mycourseville.com/api/v1/public/users/me",
-  //         profileOptions,
-  //         (profileRes) => {
-  //           let profileData = "";
-  //           profileRes.on("data", (chunk) => {
-  //             profileData += chunk;
-  //           });
-  //           profileRes.on("end", () => {
-  //             const profile = JSON.parse(profileData);
+    const tokenReq = https.request(access_token_url, options, (tokenRes) => {
+      let tokenData = "";
+      tokenRes.on("data", (chunk) => {
+        tokenData += chunk;
+      });
+      tokenRes.on("end", () => {
+        const token = JSON.parse(tokenData);
+        // Use the access token to fetch the user's profile
+        // const profileOptions = {
+        //   headers: {
+        //     Authorization: `Bearer ${token.access_token}`,
+        //   },
+        // };
+        // const profileReq = https.request(
+        //   "https://www.mycourseville.com/api/v1/public/users/me",
+        //   profileOptions,
+        //   (profileRes) => {
+        //     let profileData = "";
+        //     profileRes.on("data", (chunk) => {
+        //       profileData += chunk;
+        //     });
+        //     profileRes.on("end", () => {
+        //       const profile = JSON.parse(profileData);
 
-  //             // Redirect to the home page after successful authentication
-  //             res.writeHead(302, { Location: "/" });
-  //             // res.send(profile)
-  //             res.end();
-  //           });
-  //         }
-  //       );
-  //       profileReq.on("error", (err) => {
-  //         console.error(err);
-  //       });
-  //       profileReq.end();
-  //       // res.send(token)
-  //       // res.end();
-  //     });
-  //   });
-  //   tokenReq.on("error", (err) => {
-  //     console.error(err);
-  //   });
-  //   tokenReq.write('success');
-  //   tokenReq.end();
-  // } else {
-  //   // If the user hasn't granted or denied the authorization request yet, redirect to the authorization URL
-  //   res.writeHead(302, { Location: authorization_url });
-  //   res.end();
-  // }
+        //       // Redirect to the home page after successful authentication
+        //       res.writeHead(302, { Location: "/" });
+        //       // res.send(profile)
+        //       res.end();
+        //     });
+        //   }
+        // );
+        // profileReq.on("error", (err) => {
+        //   console.error(err);
+        // });
+        // profileReq.end();
+        res.send(token)
+        res.end();
+      });
+    });
+    tokenReq.on("error", (err) => {
+      console.error(err);
+    });
+    // tokenReq.write('success');
+    tokenReq.end();
+  } else {
+    // If the user hasn't granted or denied the authorization request yet, redirect to the authorization URL
+    res.writeHead(302, { Location: authorization_url });
+    res.end();
+  }
 };
