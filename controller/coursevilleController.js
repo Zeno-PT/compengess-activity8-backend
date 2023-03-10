06a9a1945a06bfa0b202ea5dcfcb40ddf4060121
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-11 01:59:53 +0700

    Update coursevilleController.js

--- controller/coursevilleController.js
+++ controller/coursevilleController.js
@@ -54,8 +54,9 @@ exports.accessToken = (req, res) => {
           req.session.token = token;
           // Redirect to your home.html page in frontend
           // TODO: Change to EC2 frontend-cv-api-XX public IP later when deployed.
-          console.log(req.session.token.access_token)
-          res.redirect('http://127.0.0.1:5500/login_cv/home.html')
+          // console.log(req.session.token.access_token)
+          // res.redirect('http://127.0.0.1:5500/login_cv/home.html')
+          location.href = "http://127.0.0.1:5500/login_cv/home.html"
           res.end();
         });
       }
