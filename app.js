271097cfeb2ab454f0f85b69424054b1a24cc459
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-10 21:44:24 +0700

    fix

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(express.static('static'))
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/items", itemsRoutes);
 app.use("/courseville", coursevilleRoutes)
 app.get('/', (req, res) => {
