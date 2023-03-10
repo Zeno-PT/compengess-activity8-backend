6722705cd38c5d38c3f5aa1dce082b3e48a38955
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-11 01:17:51 +0700

    Update coursevilleController.js

--- controller/coursevilleController.js
+++ controller/coursevilleController.js
@@ -1,7 +1,3 @@
-const https = require("https");
-const url = require("url");
-const querystring = require("querystring");
-
 // TODO: Change cilentId, clientSecret, redirectUri below
 const client_id = "PWIuxwNVlZh70gSnWMoxfWcFpg5c7Odk1MLx3wSA";
 const client_secret = "yC5OC38phIdKrBBqCvrbyuxy0TZbGDkrZmokp9Ke";
@@ -10,12 +6,9 @@ const redirect_uri = "http://44.214.169.149:3000/courseville/access_token";
 const authorization_url = `https://www.mycourseville.com/api/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}`;
 const access_token_url = "https://www.mycourseville.com/api/oauth/access_token";
 
-const token = {
-  access_token: "oo4Ko8lK1UBaVnoiWJBRVFusipdgfIet1cp48rlq",
-  token_type: "Bearer",
-  expires_in: 1209600,
-  refresh_token: "ghBJHsbN9qKCquMltycX8cRFf8XkwTLT6geyx2FN",
-};
+const https = require("https");
+const url = require("url");
+const querystring = require("querystring");
 
 exports.authApp = async (req, res) => {
   res.redirect(authorization_url);
@@ -25,16 +18,13 @@ exports.accessToken = async (req, res) => {
   const parsedUrl = url.parse(req.url);
   const parsedQuery = querystring.parse(parsedUrl.query);
 
-  // If the user denied the authorization request
   if (parsedQuery.error) {
     res.writeHead(400, { "Content-Type": "text/plain" });
     res.end(`Authorization error: ${parsedQuery.error_description}`);
     return;
   }
 
-  // If the user granted the authorization request
   if (parsedQuery.code) {
-    // Exchange the authorization code for an access token
     const postData = querystring.stringify({
       grant_type: "authorization_code",
       code: parsedQuery.code,
@@ -55,16 +45,14 @@ exports.accessToken = async (req, res) => {
       access_token_url,
       tokenOptions,
       (tokenRes) => {
-        // res.send("OK");
-        // res.end();
         let tokenData = "";
         tokenRes.on("data", (chunk) => {
           tokenData += chunk;
         });
         tokenRes.on("end", () => {
           const token = JSON.parse(tokenData);
-          req.session.access_token = token;
-          res.send(token);
+          req.session.token = token;
+          res.redirect('https://www.google.com')
           res.end();
         });
       }
@@ -83,7 +71,7 @@ exports.accessToken = async (req, res) => {
 exports.getProfileInformation = async (req, res) => {
   const profileOptions = {
     headers: {
-      Authorization: `Bearer ${token.access_token}`,
+      Authorization: `Bearer ${req.session.token.access_token}`,
     },
   };
   const profileReq = https.request(
@@ -96,9 +84,6 @@ exports.getProfileInformation = async (req, res) => {
       });
       profileRes.on("end", () => {
         const profile = JSON.parse(profileData);
-
-        // Redirect to the home page after successful authentication
-        // res.writeHead(302, { Location: "/" });
         res.send(profile);
         res.end();
       });
@@ -113,7 +98,7 @@ exports.getProfileInformation = async (req, res) => {
 exports.getCourses = async (req, res) => {
   const courseOptions = {
     headers: {
-      Authorization: `Bearer ${token.access_token}`,
+      Authorization: `Bearer ${req.session.token.access_token}`,
     },
   };
   const courseReq = https.request(
@@ -140,7 +125,7 @@ exports.getCourses = async (req, res) => {
 exports.getCompEngEssAssignments = async (req, res) => {
   const assignmentOptions = {
     headers: {
-      Authorization: `Bearer ${token.access_token}`,
+      Authorization: `Bearer ${req.session.token.access_token}`,
     },
   };
   const assignmentReq = https.request(
