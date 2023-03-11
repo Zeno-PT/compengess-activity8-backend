ad2b561db6dbad735cf55095d2ffe0f92c74a0cf
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-11 15:20:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const AppError = require("./utils/appError")
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const AppError = require("./utils/appError");
 const itemsRoutes = require("./routes/itemRoutes");
-const coursevilleRoutes = require("./routes/coursevilleRoutes")
+const coursevilleRoutes = require("./routes/coursevilleRoutes");
 
 const app = express();
 
-app.use(session({secret: 'my-secret', resave: false, saveUninitialized: false}))
-app.use(cors());
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
-app.use(express.static('static'))
+app.use(
+  session({ secret: "my-secret", resave: false, saveUninitialized: false })
+);
+app.use(cors({ origin: "http://127.0.0.1:8000", credentials: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("static"));
 
 app.use("/items", itemsRoutes);
-app.use("/courseville", coursevilleRoutes)
-app.get('/', (req, res) => {
-  res.send('Congratulation. This server is succesfully run.')
-})
+app.use("/courseville", coursevilleRoutes);
+app.get("/", (req, res) => {
+  res.send("Congratulation. This server is succesfully run.");
+});
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
