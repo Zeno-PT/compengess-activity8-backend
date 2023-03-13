e7ae025f1b36086f97e8f49f9142bf046c1e0f23
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-13 14:35:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const corsOptions = {
 };
 
 app.use(session(session_option));
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("static"));
