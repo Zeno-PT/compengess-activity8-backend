e6f0daf418941e374e485210f5fbf0bb969a8627
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-14 23:38:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,11 +33,11 @@ const corsOptions = {
   credentials: true // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
+app.use(express.static("static"));
 app.use(session(sessionOptions));
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("static"));
 
 app.use("/items", itemsRoutes);
 app.use("/courseville", coursevilleRoutes);
