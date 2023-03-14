dd0bde08032e13a9c433ebba281271fa77d02e55
Zeno-PT <60151629+Zeno-PT@users.noreply.github.com>
2023-03-14 23:33:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const app = require("./app");
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {});
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
